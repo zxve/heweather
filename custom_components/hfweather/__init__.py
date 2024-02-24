e9807a7213e4d0c4a58d1d6228b94f871d263a8b
zxve <1785069246@qq.com>
2024-02-24 17:40:35 +0800

    x

--- custom_components/hfweather/__init__.py
+++ custom_components/hfweather/__init__.py
@@ -1,30 +1,28 @@
 import asyncio
-import requests
-import json
-import datetime
+# import requests
+# import json
+# import datetime
 import logging
-
-from aiohttp.client_exceptions import ClientConnectorError
-from async_timeout import timeout
-
-from homeassistant.const import CONF_API_KEY
+#
+# from aiohttp.client_exceptions import ClientConnectorError
+# from async_timeout import timeout
+#
+# from homeassistant.const import CONF_API_KEY
 from homeassistant.core import Config, HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-from homeassistant.util.unit_system import METRIC_SYSTEM
+# from homeassistant.exceptions import ConfigEntryNotReady
+# from homeassistant.helpers.aiohttp_client import async_get_clientsession
+# from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+# from homeassistant.util.unit_system import METRIC_SYSTEM
 
 from .const import (
     CONF_API_VERSION,
     COORDINATOR,
     DOMAIN,
-    UNDO_UPDATE_LISTENER, CONF_LOCATION, CONF_LONGITUDE, CONF_LATITUDE,
+    UNDO_UPDATE_LISTENER, CONF_LOCATION, CONF_LONGITUDE, CONF_LATITUDE, PLATFORMS,
 )
 
 _LOGGER = logging.getLogger(__name__)
 # _LOGGER.info("zxve 000")
-
-PLATFORMS = ["sensor", "weather"]
 
 
 async def async_setup(hass: HomeAssistant, config: Config) -> bool:
@@ -32,40 +30,40 @@ async def async_setup(hass: HomeAssistant, config: Config) -> bool:
     return True
 
 
-async def async_setup_entry(hass, config_entry) -> bool:
-    try:
-        api_key = config_entry.data[CONF_API_KEY]
-        location_key = config_entry.unique_id
-        longitude = config_entry.data[CONF_LONGITUDE]
-        latitude = config_entry.data[CONF_LATITUDE]
-        api_version = config_entry.data[CONF_API_VERSION]
-
-        _LOGGER.debug("Using location_key: %s, get forecast: %s", location_key, api_version)
-
-        websession = async_get_clientsession(hass)
-
-        coordinator = HfweatherDataUpdateCoordinator(hass, websession, api_key, api_version, location_key, longitude,
-                                                     latitude)
-        await coordinator.async_refresh()
-
-        if not coordinator.last_update_success:
-            raise ConfigEntryNotReady
-
-        undo_listener = config_entry.add_update_listener(update_listener)
-
-        hass.data[DOMAIN][config_entry.entry_id] = {
-            COORDINATOR: coordinator,
-            UNDO_UPDATE_LISTENER: undo_listener,
-        }
-
-        for component in PLATFORMS:
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(config_entry, component)
-            )
-
-        return True
-    except Exception as e:
-        raise e
+# async def async_setup_entry(hass, config_entry) -> bool:
+#     try:
+#         api_key = config_entry.data[CONF_API_KEY]
+#         location_key = config_entry.unique_id
+#         longitude = config_entry.data[CONF_LONGITUDE]
+#         latitude = config_entry.data[CONF_LATITUDE]
+#         api_version = config_entry.data[CONF_API_VERSION]
+#
+#         _LOGGER.debug("Using location_key: %s, get forecast: %s", location_key, api_version)
+#
+#         websession = async_get_clientsession(hass)
+#
+#         coordinator = HfweatherDataUpdateCoordinator(hass, websession, api_key, api_version, location_key, longitude,
+#                                                      latitude)
+#         await coordinator.async_refresh()
+#
+#         if not coordinator.last_update_success:
+#             raise ConfigEntryNotReady
+#
+#         undo_listener = config_entry.add_update_listener(update_listener)
+#
+#         hass.data[DOMAIN][config_entry.entry_id] = {
+#             COORDINATOR: coordinator,
+#             UNDO_UPDATE_LISTENER: undo_listener,
+#         }
+#
+#         for component in PLATFORMS:
+#             hass.async_create_task(
+#                 hass.config_entries.async_forward_entry_setup(config_entry, component)
+#             )
+#
+#         return True
+#     except Exception as e:
+#         raise e
 
 
 async def async_unload_entry(hass, config_entry):
@@ -90,43 +88,43 @@ async def update_listener(hass, config_entry):
     await hass.config_entries.async_reload(config_entry.entry_id)
 
 
-class HfweatherDataUpdateCoordinator(DataUpdateCoordinator):
-    def __init__(self, hass, session, api_key, api_version, location_key, longitude, latitude):
-        self.location_key = location_key
-        self.longitude = longitude
-        self.latitude = latitude
-        self.api_version = api_version
-        self.api_key = api_key
-        self.is_metric = "metric:v2"
-        if hass.config.units is METRIC_SYSTEM:
-            self.is_metric = "metric:v2"
-        else:
-            self.is_metric = "imperial"
-
-        update_interval = (
-            datetime.timedelta(minutes=60)
-        )
-        _LOGGER.debug("Data will be update every %s", update_interval)
-
-        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
-
-    # @asyncio.coroutine
-    def get_data(self, url):
-        json_text = requests.get(url).content
-        resdata = json.loads(json_text)
-        return resdata
-
-    async def _async_update_data(self):
-        try:
-            async with timeout(10):
-                url = str.format("https://devapi.qweather.com/{}/weather/now?location={},{}&key={}", self.api_version,
-                                 self.longitude, self.latitude, self.api_key)
-                # json_text = requests.get(url).content
-                resdata = await self.hass.async_add_executor_job(self.get_data, url)
-                # _LOGGER.info(resdata)
-        except (
-                ClientConnectorError
-        ) as error:
-            raise UpdateFailed(error)
-        _LOGGER.debug("Requests remaining: %s", url)
-        return {**resdata, "location_key": self.location_key, "is_metric": self.is_metric}
+# class HfweatherDataUpdateCoordinator(DataUpdateCoordinator):
+#     def __init__(self, hass, session, api_key, api_version, location_key, longitude, latitude):
+#         self.location_key = location_key
+#         self.longitude = longitude
+#         self.latitude = latitude
+#         self.api_version = api_version
+#         self.api_key = api_key
+#         self.is_metric = "metric:v2"
+#         if hass.config.units is METRIC_SYSTEM:
+#             self.is_metric = "metric:v2"
+#         else:
+#             self.is_metric = "imperial"
+#
+#         update_interval = (
+#             datetime.timedelta(minutes=60)
+#         )
+#         _LOGGER.debug("Data will be update every %s", update_interval)
+#
+#         super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
+#
+#     # @asyncio.coroutine
+#     def get_data(self, url):
+#         json_text = requests.get(url).content
+#         resdata = json.loads(json_text)
+#         return resdata
+#
+#     async def _async_update_data(self):
+#         try:
+#             async with timeout(10):
+#                 url = str.format("https://devapi.qweather.com/{}/weather/now?location={},{}&key={}", self.api_version,
+#                                  self.longitude, self.latitude, self.api_key)
+#                 # json_text = requests.get(url).content
+#                 resdata = await self.hass.async_add_executor_job(self.get_data, url)
+#                 # _LOGGER.info(resdata)
+#         except (
+#                 ClientConnectorError
+#         ) as error:
+#             raise UpdateFailed(error)
+#         _LOGGER.debug("Requests remaining: %s", url)
+#         return {**resdata, "location_key": self.location_key, "is_metric": self.is_metric}
--- custom_components/hfweather/const.py
+++ custom_components/hfweather/const.py
@@ -1,15 +1,26 @@
+from datetime import timedelta
+import homeassistant.util.dt as dt_util
+
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import (
     ATTR_DEVICE_CLASS,
     UnitOfLength,
     UnitOfTemperature,
-    DEGREE, UnitOfSpeed
+    DEGREE, UnitOfSpeed,
+    TEMP_CELSIUS,
+    CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    PERCENTAGE,
+    PRECIPITATION_MILLIMETERS_PER_HOUR,
+    SPEED_KILOMETERS_PER_HOUR,
+    PRESSURE_HPA,
+    LENGTH_KILOMETERS,
 )
 
 # 方便直接使用Home Assistant Brands上面的和风天气图标, 在这里和manifest.json中修改domain为heweather
 DOMAIN = "heweather"
 
-PLATFORMS = ["sensor"]
+PLATFORMS = ["sensor", "weather"]
+
 REQUIRED_FILES = [
     "const.py",
     "manifest.json",
@@ -24,7 +35,8 @@ ISSUE_URL = ""
 STARTUP = """
 
 """
-
+ATTR_UPDATE_TIME = "更新时间"
+ATTR_SUGGESTION = "建议"
 ATTRIBUTION = "Data provided by 和风天气"
 ATTR_ICON = "icon"
 ATTR_FORECAST = CONF_DAILYSTEPS = "forecast"
@@ -48,6 +60,88 @@ OPTIONAL_SENSORS = (
     "WindDirection",
 )
 
+# new
+DISASTER_LEVEL = {
+    "Cancel": 0,
+    "None": 0,
+    "Unknown": 0,
+    "Standard": 1,
+    "Minor": 2,
+    "Moderate": 3,
+    "Major": 4,
+    "Severe": 5,
+    "Extreme": 6
+}
+
+WEATHER_TIME_BETWEEN_UPDATES = timedelta(seconds=600)
+LIFE_SUGGESTION_TIME_BETWEEN_UPDATES = timedelta(seconds=7200)
+
+TIME_BETWEEN_UPDATES = timedelta(seconds=1800)
+HOURLY_TIME_BETWEEN_UPDATES = timedelta(seconds=1800)
+
+DEFAULT_TIME = dt_util.now()
+CONF_OPTIONS = "options"
+CONF_LOCATION = "location"
+CONF_KEY = "key"
+CONF_DISASTER_LEVEL = "disaster_level"
+CONF_DISASTER_MSG = "disaster_msg"
+
+OPTIONS = {
+    "temperature": ["Heweather_temperature", "室外温度", "mdi:thermometer", TEMP_CELSIUS],
+    "humidity": ["Heweather_humidity", "室外湿度", "mdi:water-percent", PERCENTAGE],
+    "feelsLike": ["Heweather_feelsLike", "体感温度", "mdi:thermometer", TEMP_CELSIUS],
+    "text": ["Heweather_text", "天气描述", "mdi:thermometer", ' '],
+    "precip": ["Heweather_precip", "小时降水量", "mdi:weather-rainy", PRECIPITATION_MILLIMETERS_PER_HOUR],
+    "windDir": ["Heweather_windDir", "风向", "mdi:windsock", ' '],
+    "windScale": ["Heweather_windScale", "风力等级", "mdi:weather-windy", ' '],
+    "windSpeed": ["Heweather_windSpeed", "风速", "mdi:weather-windy", SPEED_KILOMETERS_PER_HOUR],
+    "dew": ["Heweather_dew", "露点温度", "mdi:thermometer-water", ' '],
+    "pressure": ["Heweather_pressure", "大气压强", "mdi:thermometer", PRESSURE_HPA],
+    "vis": ["Heweather_vis", "能见度", "mdi:thermometer", LENGTH_KILOMETERS],
+    "cloud": ["Heweather_cloud", "云量", "mdi:cloud-percent", PERCENTAGE],
+    "primary": ["Heweather_primary", "空气质量的主要污染物", "mdi:weather-dust", " "],
+    "category": ["Heweather_category", "空气质量指数级别", "mdi:walk", " "],
+    "level": ["Heweather_level", "空气质量指数等级", "mdi:walk", " "],
+    "pm25": ["Heweather_pm25", "PM2.5", "mdi:walk", CONCENTRATION_MICROGRAMS_PER_CUBIC_METER],
+    "pm10": ["Heweather_pm10", "PM10", "mdi:walk", CONCENTRATION_MICROGRAMS_PER_CUBIC_METER],
+    "no2": ["Heweather_no2", "二氧化氮", "mdi:emoticon-dead", CONCENTRATION_MICROGRAMS_PER_CUBIC_METER],
+    "so2": ["Heweather_so2", "二氧化硫", "mdi:emoticon-dead", CONCENTRATION_MICROGRAMS_PER_CUBIC_METER],
+    "co": ["Heweather_co", "一氧化碳", "mdi:molecule-co", CONCENTRATION_MICROGRAMS_PER_CUBIC_METER],
+    "o3": ["Heweather_o3", "臭氧", "mdi:weather-cloudy", CONCENTRATION_MICROGRAMS_PER_CUBIC_METER],
+    "qlty": ["Heweather_qlty", "综合空气质量", "mdi:quality-high", " "],
+    "disaster_warn": ["Heweather_disaster_warn", "灾害预警", "mdi:alert", " "],
+    "air": ["suggestion_air", "空气污染扩散条件指数", "mdi:air-conditioner", " "],
+    "comfortable": ["suggestion_comf", "舒适度指数", "mdi:human-greeting", " "],
+    "cw": ["suggestion_cw", "洗车指数", "mdi:car", " "],
+    "drsg": ["suggestion_drsg", "穿衣指数", "mdi:hanger", " "],
+    "flu": ["suggestion_flu", "感冒指数", "mdi:biohazard", " "],
+    "sport": ["suggestion_sport", "运动指数", "mdi:badminton", " "],
+    "trav": ["suggestion_trav", "旅行指数", "mdi:wallet-travel", " "],
+    "uv": ["suggestion_uv", "紫外线指数", "mdi:weather-sun-wireless", " "],
+    "guomin": ["suggestion_guomin", "过敏指数", "mdi:sunglasses", " "],
+    "kongtiao": ["suggestion_kongtiao", "空调开启指数", "mdi:air-conditioner", " "],
+    "sunglass": ["suggestion_sunglass", "太阳镜指数", "mdi:sunglasses", " "],
+    "fangshai": ["suggestion_fangshai", "防晒指数", "mdi:sun-protection-outline", " "],
+    "liangshai": ["suggestion_liangshai", "晾晒指数", "mdi:tshirt-crew-outline", " "],
+    "jiaotong": ["suggestion_jiaotong", "交通指数", "mdi:train-car", " "],
+}
+
+CONDITION_CLASSES = {
+    'sunny': ["晴"],
+    'cloudy': ["多云"],
+    'partlycloudy': ["少云", "晴间多云", "阴"],
+    'windy': ["有风", "微风", "和风", "清风"],
+    'windy-variant': ["强风", "劲风", "疾风", "大风", "烈风"],
+    'hurricane': ["飓风", "龙卷风", "热带风暴", "狂暴风", "风暴"],
+    'rainy': ["雨", "毛毛雨", "细雨", "小雨", "小到中雨", "中雨", "中到大雨", "大雨", "大到暴雨", "阵雨", "极端降雨", "冻雨"],
+    'pouring': ["暴雨", "暴雨到大暴雨", "大暴雨", "大暴雨到特大暴雨", "特大暴雨", "强阵雨"],
+    'lightning-rainy': ["雷阵雨", "强雷阵雨"],
+    'fog': ["雾", "薄雾", "霾", "浓雾", "强浓雾", "中度霾", "重度霾", "严重霾", "大雾", "特强浓雾"],
+    'hail': ["雷阵雨伴有冰雹"],
+    'snowy': ["小雪", "小到中雪", "中雪", "中到大雪", "大雪", "大到暴雪", "暴雪", "阵雪"],
+    'snowy-rainy': ["雨夹雪", "雨雪天气", "阵雨夹雪"],
+    'exceptional': ["扬沙", "浮尘", "沙尘暴", "强沙尘暴", "未知"],
+}
 SENSOR_TYPES = {
     "temperature": {
         ATTR_DEVICE_CLASS: SensorDeviceClass.TEMPERATURE,
