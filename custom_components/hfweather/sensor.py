f4a26f57caf77bb8dec3b13386400c933c15eb6c
zxve <1785069246@qq.com>
2024-03-29 23:44:59 +0800

    x

--- custom_components/hfweather/sensor.py
+++ custom_components/hfweather/sensor.py
@@ -59,7 +59,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         raise e
 
 
-class HfweatherSensor(CoordinatorEntity, SensorEntity):
+class HfweatherSensor(SensorEntity):
     """定义一个温度传感器的类，继承自HomeAssistant的Entity类."""
 
     def __init__(self, name, option, coordinator):
